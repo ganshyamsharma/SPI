6b3c77116d38022eaff11c84dd88d5709407210f
ganshyamsharma <gsmsharma99@gmail.com>
2025-05-25 19:22:12 +0530

    fixed errors related to data latching

--- SPI Main/spi_main.v
+++ SPI Main/spi_main.v
@@ -1,28 +1,44 @@
-
 module spi_main(
 	input i_clk, i_com_start, i_mode_sel,
 	input [1:0] i_mode,
 	input [7:0] i_tx_byte,
 	input i_miso,
 	output reg o_mosi,
-	output o_cs_n, o_clk, o_done,
-	output [7:0] rx_byte
+	output o_cs_n, o_clk, o_tx_done, o_rx_done,							
+	output reg [7:0] o_rx_byte
 	);
-		localparam mode0 = 0;
-		localparam mode1 = 1;
-		localparam mode2 = 2;
-		localparam mode3 = 3;
+		localparam mode0 = 2'd0;
+		localparam mode1 = 2'd1;
+		localparam mode2 = 2'd2;
+		localparam mode3 = 2'd3;
 		
 		reg [7:0] r_rx_byte = 0;
 		reg [7:0] r_tx_byte = 0;
 		reg [1:0] r_mode = 0;
 		reg [3:0] r_bit_index_tx = 0, r_bit_index_rx = 0;
-		reg r_cs_n = 1'b1, r_clk, r_done;
-		
+		reg r_cs_n = 1'b1, r_clk, r_tx_done = 0, r_rx_done = 0;
+		//
+		///////////////////////////////////Output Assignments/////////////////////////////////
+		//
 		assign o_cs_n = r_cs_n;
 		assign o_clk = r_clk;
-		assign o_done = r_done;
-		
+		assign o_tx_done = r_tx_done;
+		assign o_rx_done = r_rx_done;
+		//
+		///////////////////////Latching the Input Byte Data for Transmission//////////////////
+		//
+		always @(posedge r_tx_done) begin
+			r_tx_byte <= i_tx_byte;
+		end
+		//
+		////////////////////////////Latching the Received Byte Data///////////////////////////
+		//
+		always @(posedge r_rx_done) begin
+			o_rx_byte <= r_rx_byte;
+		end
+		//
+		/////////////////Posedge Shifting and Sampling based on Mode Selected////////////////
+		//
 		always @(posedge i_clk) begin
 			if(r_cs_n) begin
 				if(i_mode_sel)
@@ -34,17 +50,21 @@ module spi_main(
 			case(r_mode)
 				mode0:
 					begin
-						if(i_com_start) 											////sampling
+						if(i_com_start) 										    ////sampling
 							begin
 								r_clk <= i_clk;
 								r_cs_n <= 0;
 								r_rx_byte[r_bit_index_rx] <= i_miso;
 								if(r_bit_index_rx < 7)
-									r_bit_index_rx <= r_bit_index_rx + 1;
-									r_done <= 0;///////////////////////
+									begin
+										r_bit_index_rx <= r_bit_index_rx + 1;
+										r_rx_done <= 0;								
+									end
 								else
-									r_done <= 1;//////////////////////
-									r_bit_index_rx <= 0;
+									begin
+										r_rx_done <= 1;								
+										r_bit_index_rx <= 0;
+									end
 							end
 						else 
 							begin
@@ -60,9 +80,15 @@ module spi_main(
 								r_cs_n <= 0;
 								o_mosi <= r_tx_byte[r_bit_index_tx];		
 								if(r_bit_index_tx < 7)
-									r_bit_index_tx <= r_bit_index_tx + 1;
-								else
-									r_bit_index_tx <= 0;
+									begin
+										r_bit_index_tx <= r_bit_index_tx + 1;
+										r_tx_done <= 0;
+									end
+								else 
+									begin
+										r_bit_index_tx <= 0;
+										r_tx_done <= 1;
+									end
 							end
 						else 
 							begin
@@ -78,9 +104,15 @@ module spi_main(
 								r_cs_n <= 0;
 								o_mosi <= r_tx_byte[r_bit_index_tx];		
 								if(r_bit_index_tx < 7)
-									r_bit_index_tx <= r_bit_index_tx + 1;
-								else
-									r_bit_index_tx <= 0;
+									begin
+										r_bit_index_tx <= r_bit_index_tx + 1;
+										r_tx_done <= 0;
+									end
+								else 
+									begin
+										r_bit_index_tx <= 0;
+										r_tx_done <= 1;
+									end
 							end
 						else 
 							begin
@@ -96,9 +128,15 @@ module spi_main(
 								r_cs_n <= 0;
 								r_rx_byte[r_bit_index_rx] <= i_miso;
 								if(r_bit_index_rx < 7)
-									r_bit_index_rx <= r_bit_index_rx + 1;
+									begin
+										r_bit_index_rx <= r_bit_index_rx + 1;
+										r_rx_done <= 0;								
+									end
 								else
-									r_bit_index_rx <= 0;
+									begin
+										r_rx_done <= 1;								
+										r_bit_index_rx <= 0;
+									end
 							end
 						else 
 							begin
@@ -108,7 +146,9 @@ module spi_main(
 					end			
 			endcase			
 		end
-	
+		//
+		/////////////////Negedge Shifting and Sampling based on Mode Selected////////////////
+		//
 		always @(negedge i_clk) begin
 			case(r_mode)
 				mode0:
@@ -119,13 +159,19 @@ module spi_main(
 								r_cs_n <= 0;
 								o_mosi <= r_tx_byte[r_bit_index_tx];				/////shifting
 								if(r_bit_index_tx < 7)
-									r_bit_index_tx <= r_bit_index_tx + 1;
-								else
-									r_bit_index_tx <= 0;
+									begin
+										r_bit_index_tx <= r_bit_index_tx + 1;
+										r_tx_done <= 0;
+									end
+								else 
+									begin
+										r_bit_index_tx <= 0;
+										r_tx_done <= 1;
+									end
 							end
 						else 
 							begin
-								r_clk <= 0;
+								//r_clk <= 0;
 								r_cs_n <= 1;
 							end
 					end
@@ -137,14 +183,19 @@ module spi_main(
 								r_cs_n <= 0;
 								r_rx_byte[r_bit_index_rx] <= i_miso;				/////sampling
 								if(r_bit_index_rx < 7)
-									r_bit_index_rx <= r_bit_index_rx + 1;
+									begin
+										r_bit_index_rx <= r_bit_index_rx + 1;
+										r_rx_done <= 0;								
+									end
 								else
-									r_bit_index_rx <= 0;
-									r_cs_n <= 1;
+									begin
+										r_rx_done <= 1;								
+										r_bit_index_rx <= 0;
+									end
 							end
 						else 
 							begin
-								r_clk <= 0;
+								//r_clk <= 0;
 								r_cs_n <= 1;
 							end
 					end
@@ -156,13 +207,19 @@ module spi_main(
 								r_cs_n <= 0;
 								r_rx_byte[r_bit_index_rx] <= i_miso;				/////sampling
 								if(r_bit_index_rx < 7)
-									r_bit_index_rx <= r_bit_index_rx + 1;
+									begin
+										r_bit_index_rx <= r_bit_index_rx + 1;
+										r_rx_done <= 0;								
+									end
 								else
-									r_bit_index_rx <= 0;
+									begin
+										r_rx_done <= 1;								
+										r_bit_index_rx <= 0;
+									end
 							end
 						else 
 							begin
-								r_clk <= 1;
+								//r_clk <= 1;
 								r_cs_n <= 1;
 							end
 					end
@@ -174,15 +231,22 @@ module spi_main(
 								r_cs_n <= 0;
 								o_mosi <= r_tx_byte[r_bit_index_tx];				/////shifting
 								if(r_bit_index_tx < 7)
-									r_bit_index_tx <= r_bit_index_tx + 1;
-								else
-									r_bit_index_tx <= 0;
+									begin
+										r_bit_index_tx <= r_bit_index_tx + 1;
+										r_tx_done <= 0;
+									end
+								else 
+									begin
+										r_bit_index_tx <= 0;
+										r_tx_done <= 1;
+									end
 							end
 						else 
 							begin
-								r_clk <= 1;
+								//r_clk <= 1;
 								r_cs_n <= 1;
 							end
 					end
 			endcase
 		end
+endmodule
